d4661846d0d87f8da8c8d04135c3809339a2e4a3
Amrat kumar <83054299+amratkumar134@users.noreply.github.com>
2022-04-10 20:38:13 +0500

    Add files via upload

---
+++ forts/script.js
@@ -0,0 +1,19 @@
+function toggleTheme(){
+    document.querySelector(".places").classList.toggle("dark");
+    document.querySelector(".place_card1").classList.toggle("dark_card");
+    document.querySelector(".place_card2").classList.toggle("dark_card");
+    document.querySelector(".place_card3").classList.toggle("dark_card");
+
+    document.querySelector(".place_card4").classList.toggle("dark_card");
+
+    document.querySelector(".place_card5").classList.toggle("dark_card");
+
+    document.querySelector(".place_card6").classList.toggle("dark_card");
+    document.querySelector(".place_card7").classList.toggle("dark_card");
+
+
+
+}
+function changeText(){
+    document.querySelector(".btn").changeText="djhdwjdhwkdhwu"
+}
